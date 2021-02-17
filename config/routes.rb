d139399b425c7490acd0203ed2f96b11cb9663a5
Malik Lechekhab <lechekhab.m@gmail.com>
2021-02-17 22:06:14 +0100

    add weeks scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :weeks
   devise_for :users
   resources :students
   get 'home/about'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_194216) do
+ActiveRecord::Schema.define(version: 2021_02_17_193004) do
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2021_02_15_194216) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "weeks", force: :cascade do |t|
+    t.integer "num"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
