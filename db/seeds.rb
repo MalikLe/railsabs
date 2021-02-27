dd6483ee2998d376be79ae0999f9cf89d0dc7566
Malik Lechekhab <lechekhab.m@gmail.com>
2021-02-27 12:34:18 +0100

    Added weekend to the date intervals

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Week.where( id: 0..1000 ).delete_all
+
+start = Date.new(2021, 1, 4)
+stop = start + 6
+
+for i in 1..52
+	Week.create([{ num: i, start_date: start, end_date: stop }])
+	start = start + 7
+	stop = start + 6
+end
