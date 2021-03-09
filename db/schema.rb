f2d060afbad54e14c3b7ba311dfb389432c591f2
Malik Lechekhab <lechekhab.m@gmail.com>
2021-03-09 21:54:14 +0100

    Add birthdate column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_074738) do
+ActiveRecord::Schema.define(version: 2021_03_09_205055) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_03_07_074738) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "class_id"
+    t.date "birth_date"
     t.index ["class_id"], name: "index_students_on_class_id"
   end
 
