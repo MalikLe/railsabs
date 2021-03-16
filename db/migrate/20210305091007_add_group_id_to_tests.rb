77dc61a585b6af3b49ebd244c36c5926c45c9b87
Malik Lechekhab <lechekhab.m@gmail.com>
2021-03-16 21:53:44 +0100

    Deleted useless migration

--- db/migrate/20210305091007_add_group_id_to_tests.rb
+++
@@ -1,6 +0,0 @@
-class AddGroupIdToTests < ActiveRecord::Migration[6.1]
-  def change
-    add_column :tests, :group_id, :integer
-    add_index :tests, :group_id
-  end
-end
--- db/migrate/20210305211419_drop_tables_notes.rb
+++
@@ -1,7 +0,0 @@
-class DropTablesNotes < ActiveRecord::Migration[6.1]
-  def change
-  	drop_table :tests
-  	drop_table :subjects
-  	drop_table :results
-  end
-end
