ac6ac728d2318592d55e759d05ef2b7f88e16c0b
Malik Lechekhab <lechekhab.m@gmail.com>
2021-03-22 14:55:09 +0100

    Create/delete periods at the same time with students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!
   # GET /students or /students.json
   def index
     @students = Student.all
@@ -25,8 +25,20 @@ class StudentsController < ApplicationController
 
     respond_to do |format|
       if @student.save
+        c=0
+        Week.all.each do |week|
+          c = c + 1
+          for d in 1..5
+            for i in 1..7
+              Period.create([{ week_id: week.id, day: d, student_id: @student.id, state: 0 }])
+            end
+          end
+          puts  c.to_s + "/" + (52).to_s
+
         format.html { redirect_to @student, notice: "Student was successfully created." }
         format.json { render :show, status: :created, location: @student }
+      end
+
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @student.errors, status: :unprocessable_entity }
@@ -49,6 +61,8 @@ class StudentsController < ApplicationController
 
   # DELETE /students/1 or /students/1.json
   def destroy
+    Period.where(:student_id => @student.id).destroy_all
+
     @student.destroy
     respond_to do |format|
       format.html { redirect_to students_url, notice: "Student was successfully destroyed." }
