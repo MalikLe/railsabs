1080c564db8c20e5baa2194f3c0ce09455aa2b5c
Malik Lechekhab <lechekhab.m@gmail.com>
2021-05-10 20:47:16 +0200

    Unscoped periods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -134,7 +134,8 @@ class HomeController < ApplicationController
     end
 
     @periods = Period.where(:student_id => Student.where(:serial_num => @mat).ids).where("state > 0")
-    @agg_periods = @periods.group(['week_id', 'id'])
+    @weeks = Week.all
+    #@agg_periods = @periods.group(['week_id', 'id'])
     @agg_periods_abs= @periods.where(:state => 2).group('week_id').count
     @agg_periods_ret= @periods.where(:state => 1).group('week_id').count
     @tot_abs = @periods.where(:state => 2).count
